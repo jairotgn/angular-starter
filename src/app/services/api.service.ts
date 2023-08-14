f65da04b7d12dd60b24e48cd60dbe8de8ba0ea7a
Jairo Castro <69816188+jairotgn@users.noreply.github.com>
2023-08-14 19:42:00 +0200

    Update service
    Add header token for auth

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,25 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  // ENDPOINT PRODUCTION
+  // ENDPOINT DEVEL
   public apiUrl : string = 'http://laravel-starter-api.test/api/'
 
-  // ENDPOINT DEVEL
+  // ENDPOINT PRODUCTION
   // private apiUrl : string = 'https://project1.reformitas.es/laravel/public/api/'
+
+  // GEADERS
+  public headers!:HttpHeaders;
 
   // CONSTRUCTOR
   constructor(private http: HttpClient) {
 
+    // CHECK IF TOKEN
+    const authToken   = <string>localStorage.getItem('authToken')
+
+    // PREPARE HEADER AUTH
+    if (authToken){
+      this.headers = new HttpHeaders({
+        Authorization: `Bearer ${authToken}`
+      });
+    }
   }
 
   post(endPoint:string, data: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl+endPoint, data)
+    return this.http.post<any>(this.apiUrl+endPoint, data, {headers:this.headers })
   }
 
   get(endPoint:string): Observable<any> {
